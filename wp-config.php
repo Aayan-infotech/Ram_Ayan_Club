de7b68308ebf2ebe900656e680870516d611a733
Rishabh Chandra <rishabh.chandra@aayaninfotech.com>
2025-08-23 12:16:01 +0530

    First commit with new code

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'ram_cyan_club' );
+define( 'DB_NAME', 'ram_ayan_club' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
